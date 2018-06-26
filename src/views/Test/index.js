fb1db05fe355d115534d365e25c65a24f2dcd40e
rogerthoang <roger.trong.hoang@gmail.com>
2018-06-25 17:54:01 -0700

    added header to test page

--- src/views/Test/index.js
+++ src/views/Test/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-import { Container, Button, Text } from 'native-base';
-import { inject, observer } from 'mobx-react';
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { Container, Button, Text, Header, Body, Title } from "native-base";
+import { getStatusBarHeight } from "react-native-status-bar-height";
+import { inject, observer } from "mobx-react";
 
-
-@inject('commonStore')
+@inject("commonStore")
 @observer
 class Test extends Component {
   constructor(props) {
@@ -14,15 +15,31 @@ class Test extends Component {
     const { usStates } = this.props.commonStore;
 
     return (
-      <Container>
-        <Button>
-          <Text>Click Me! </Text>
-        </Button>
-        <Text> {usStates[6]["name"]}</Text>
-        <Text> This is a test page !</Text>
-      </Container>
+        <Container style={styles.header}>
+          <Header>
+            <Body>
+              <Title>Test Page</Title>
+            </Body>
+          </Header>
+          <Button>
+            <Text>Click Me!</Text>
+          </Button>
+          <Text> {usStates[34]["name"]}</Text>
+          <Text> {usStates[35]["name"]}</Text>
+          <Text> {usStates[36]["name"]}</Text>
+        </Container>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    paddingTop: getStatusBarHeight(),
+    height: 54 + getStatusBarHeight()
+  }
+});
 
 export default Test;
